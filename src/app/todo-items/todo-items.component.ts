a28328574f3ef40b21f7a275ab9dfc579777245f
wellwind <wellwind@mail2000.com.tw>
2016-09-23 20:23:57 +0800

    使用AppComponent的items資料顯示(*ng-for)

--- src/app/todo-items/todo-items.component.ts
+++ src/app/todo-items/todo-items.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todo-items',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-items.component.css']
 })
 export class TodoItemsComponent implements OnInit {
+
+  @Input()
+  items;
 
   constructor() { }
 
