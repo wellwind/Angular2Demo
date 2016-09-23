d0dfcc6c61da5f302ec154556944d00182bbb788
wellwind <wellwind@mail2000.com.tw>
2016-09-23 20:30:17 +0800

    TodoItem狀態

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
   items = [
-    {id: 1, text: "todo 1"},
-    {id: 2, text: "todo 2"},
-    {id: 3, text: "todo 3"}
+    {id: 1, text: "todo 1", done: true},
+    {id: 2, text: "todo 2", done: false},
+    {id: 3, text: "todo 3", done: true}
   ];
 
   addTodoItem(todoText) {
-    this.items.push({ id: this.items.length + 1, text: todoText})
+    this.items.push({ id: this.items.length + 1, text: todoText, done: false})
   }
 }
