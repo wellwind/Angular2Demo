9dd1c95d885ce14efceb7ed99211ea9d663bca05
wellwind <wellwind@mail2000.com.tw>
2016-09-23 19:56:32 +0800

    init add todo component

---
+++ src/app/add-todo/add-todo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css']
+})
+export class AddTodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
