840a98ad3957d887a0f4faacca008ccdfd51e435
wellwind <wellwind@mail2000.com.tw>
2016-10-17 12:10:13 +0800

    use @Input to get directive attribute value
    move core code to ngOnInit to get @Input value

--- src/app/bs-button.directive.ts
+++ src/app/bs-button.directive.ts
@@ -2,17 +2,22 @@ import {
   Directive,
   ElementRef,
   Input,
-  Renderer
+  Renderer,
+  OnInit
 } from '@angular/core';
 
 @Directive({
   selector: '[bsButton]'
 })
-export class BsButtonDirective {
+export class BsButtonDirective implements OnInit {
   @Input() bsButton: string;
 
-  constructor(el: ElementRef, renderer: Renderer) {
-    renderer.setElementClass(el.nativeElement, 'btn', true);
-    renderer.setElementClass(el.nativeElement, 'btn-info', true);
+  constructor(private el: ElementRef, private renderer: Renderer) {
+
+  }
+
+  ngOnInit() {
+    this.renderer.setElementClass(this.el.nativeElement, 'btn', true);
+    this.renderer.setElementClass(this.el.nativeElement, 'btn-' + this.bsButton, true);
   }
 }
