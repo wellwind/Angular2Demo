39333e79800588ccc8a26134cdc83491cce46cf1
wellwind <wellwind@mail2000.com.tw>
2017-01-22 22:57:16 +0800

    Add add-todo component test

--- src/app/add-todo/add-todo.component.spec.ts
+++ src/app/add-todo/add-todo.component.spec.ts
@@ -1,11 +1,55 @@
+
 /* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
-import { TestBed, async } from '@angular/core/testing';
 import { AddTodoComponent } from './add-todo.component';
+import { TodoService } from '../todo.service';
 
-describe('Component: AddTodo', () => {
-  it('should create an instance', () => {
-    let component = new AddTodoComponent();
+describe('AddTodoComponent', () => {
+  let component: AddTodoComponent;
+  let fixture: ComponentFixture<AddTodoComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+  let todoService: TodoService;
+
+  beforeEach(async(() => {
+    // mock todo service
+    var todoServiceMock = {
+      addTodoItem(todoText) {
+        expect(true).toBe(true);
+      }
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [AddTodoComponent],
+      imports: [
+        CommonModule,
+        FormsModule
+      ],
+      providers: [
+        { provide: TodoService, useValue: todoServiceMock }
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddTodoComponent);
+    component = fixture.componentInstance;
+
+    // todoService from the root injector
+    todoService = TestBed.get(TodoService);
+  });
+
+  it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('addItemClick function should call TodoService.addTodoItem', () =>{
+    component.addItemClick(null);
   });
 });
